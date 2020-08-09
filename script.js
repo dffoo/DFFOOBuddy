9539dd789c055c27ad8a3521531d7a23fc90916a
dffoo <jokko_7490@yahoo.com>
2020-08-10 07:57:46 +0800

    1.0.0

--- script.js
+++ script.js
@@ -1,12 +1,34 @@
 function changeText() {
-    document.getElementById('p1').innerHTML = "CHANGED!";
+    var num = Math.floor((Math.random() * 100)) + 1;
+    if(num >= 96){
+        document.getElementById('p1').innerHTML = "GOLD";
+    } else if(num >= 50) {
+        document.getElementById('p1').innerHTML = "SILVER";
+    } else {
+        document.getElementById('p1').innerHTML = "BRONZE";
+    }
     // alert("test");
 }
 
 function selectBanner(value) {
     var filename = `img/banner/${value}.jpg`;
-    document.getElementById('banner').src = filename;
-    document.getElementById('banner').classList.remove("animation");
-    void document.getElementById('banner').offsetWidth;
-    document.getElementById('banner').classList.add("animation");
+    document.getElementById('img-banner').src = filename;
+    document.getElementById('img-banner').classList.remove("animation");
+    void document.getElementById('img-banner').offsetWidth;
+    document.getElementById('img-banner').classList.add("animation");
+}
+
+function loadBanners() {
+    alert("LOL");
+    var section = document.getElementById('secBanners');
+    var tbl = document.createElement('table');
+    tbl.style.width  = '100px';
+    tbl.style.border = '1px solid black';
+
+    var tr = tbl.insertRow();
+    var td = tr.insertCell();
+    td.appendChild(document.createTextNode('Cell'));
+    td.style.border = '1px solid black';
+
+    body.appendChild(tbl);
 }
